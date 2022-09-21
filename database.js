76287c4edab2f5f05dc1e7aa8caf8a2d86037cad
Vuyelwa <vuyelwam@runninghill.co.za>
2022-09-21 16:37:04 +0200

    connect database to app

---
+++ database.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const { ATLAS_URI } = process.env;
+
+exports.connect = () => {
+  // Connecting to the database
+  mongoose
+    .connect(ATLAS_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    //   useCreateIndex: true,
+    //   useFindAndModify: false,
+    })
+    .then(() => {
+      console.log("Successfully connected to database");
+    })
+    .catch((error) => {
+      console.log("database connection failed. exiting now...");
+      console.error(error);
+      process.exit(1);
+    });
+};
